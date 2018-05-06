6b3cc954cfe2d7364a1e54d77e2b6047d8979731
simutaroman <simutaroman@gmail.com>
2018-05-06 16:49:33 +0300

    webpack configured, js and sass added

---
+++ AspNetWebpack/webpack.config.js
@@ -0,0 +1,39 @@
+﻿"use strict";
+
+const path = require('path');
+
+const bundleFileName = 'bundle';
+
+module.exports = {
+    mode: "development", /* also could be "production" or "none" */
+    entry: ['./src/index.js', './src/sass/index.scss'],
+    output: {
+        filename: bundleFileName + '.js',
+        path: path.resolve(__dirname, 'wwwroot/dist')
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: bundleFileName + '.css'
+                    }
+                },
+                {
+                    loader: 'extract-loader'
+                },
+                {
+                    loader: "css-loader",
+                    options: {
+                        minimize: true || {/* or CSSNano Options see http://cssnano.co/guides/ */ }
+                    }
+                },
+                {
+                    loader: "sass-loader"
+                }
+            ]
+        }]
+    }
+};
